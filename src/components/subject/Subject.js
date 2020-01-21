5c6b9d2d4f7dec55b9f64a492fef73607f2482f3
You Song Hou <yousonghou@yahoo.com>
2020-01-21 00:35:16 -0500

    add styles to the subject page

--- src/components/subject/Subject.js
+++ src/components/subject/Subject.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 
 const Subject = () => {
     return(
         <View>
             <Text style={styles.title}>Choose your subject</Text>
+            <View style={styles.container}>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Javascript</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>React</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>CSS</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>HTML</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Node</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Python</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
@@ -13,6 +33,26 @@ export default Subject;
 
 const styles = StyleSheet.create({
     title: {
-        fontSize: 50
+        marginTop: 25,
+        fontSize: 30,
+        textAlign: "center"
+    },
+    container: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        flexWrap: "wrap",
+        padding: 20
+    },
+    button: {
+        width: "40%",
+        backgroundColor: "#64EE7A",
+        paddingVertical: 20,
+        borderRadius: 10,
+        marginVertical: 10
+    },
+    buttonText: {
+        fontSize: 20,
+        textAlign: "center"
     }
 });
